e75d7c66bd61ad94c4f61b47212a7e813396b649
Pasindu Rashmika <devRushmika@gmail.com>
2026-01-21 16:56:23 +0530

    implemented an image captioning model

--- app.py
+++ app.py
@@ -1,27 +1,38 @@
 import streamlit as st
+from PIL import Image
 from transformers import pipeline
 
 st.set_page_config(page_title="AI Summarization Tool")
 
 @st.cache_resource
-def load_model():
+def load_summarization_model():
     return pipeline("summarization", model="facebook/bart-large-cnn")
 
-summarizer_model = load_model()
-st.title("Text Summarization Tool")
+@st.cache_resource
+def load_captioning_model():
+    return pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+
+
+captioning_model = load_captioning_model()
+st.title("Image Captioning Tool")
 col1, col2 = st.columns([2, 1])
 
 with col1:
-    user_input = st.text_area("Enter text to summarize: ", height=200)
-    summarizer_button = st.button("Summarize Text", type="primary")
+    uploaded_image = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+    caption_generator = st.button("Generate Captions", type="primary")
 
 with col2:
     st.markdown("Powered by Pasindu Rashmika")
 
-if summarizer_button and user_input:
-    with st.spinner("Summarizing..."):
-        result = summarizer_model(user_input)
-        summary_txt = result[0]["summary_text"]
+if uploaded_image and caption_generator:
+    with st.spinner("Generating Captions..."):
+        image = Image.open(uploaded_image).convert("RGB")
+        st.image(image, use_container_width=True)
+        result = captioning_model(image)
+        gen_text = result[0]["generated_text"]
 
-elif summarizer_button:
-    st.warning("Please enter some text to summarize!")
+
+        st.markdown(gen_text)
+
+elif caption_generator:
+    st.warning("Please upload an image first.")
